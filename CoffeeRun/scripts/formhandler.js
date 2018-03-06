1c5d3004c387e9fdf305e82bd2819ca5cec9e630
emilyemilyb <32493868+emilyemilyb@users.noreply.github.com>
2018-03-05 16:55:31 -0800

    Add files via upload

---
+++ CoffeeRun/scripts/formhandler.js
@@ -0,0 +1,40 @@
+(function (window) {
+  'use strict';
+
+  var App = window.App || {};
+  var $ = window.jQuery;
+  var $modalBody = $('#myModal .modal-body');
+
+  function FormHandler(selector) {
+    if (!selector) {
+      throw new Error('No selector provided');
+    }
+
+    this.$formElement = $(selector);
+    if (this.$formElement.length === 0) {
+      throw new Error('Could not find element with selector: ' + selector);
+    }
+  }
+
+  FormHandler.prototype.addSubmitHandler = function (fn) {
+    console.log('Setting submit handler for form');
+    this.$formElement.on('submit', function (event) {
+      event.preventDefault();
+
+      var data = {};
+      $(this).serializeArray().forEach(function (item) {
+        data[item.name] = item.value;
+        console.log(item.name + ' is ' + item.value);
+        $modalBody.text(data.user);
+      });
+      console.log(data);
+      fn(data);
+      $('#myModal').modal('show');
+      this.reset();
+      this.elements[0].focus();
+    });
+  };
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+})(window);
